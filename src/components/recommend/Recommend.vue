4fc629fe69f32665edd24a64b81502a3290b10b6
mengxiang <1547719290@qq.com>
2020-02-08 18:27:38 +0800

    轮播图片正确显示

--- src/components/recommend/Recommend.vue
+++ src/components/recommend/Recommend.vue
@@ -1,32 +1,53 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="item in slider" :key="item.id">{{item.picUrl}}</li>
-    </ul>
+  <div class="recommend">
+    <div v-if="sliderData.length" class="slider-wrapper">
+      <slider>
+        <div v-for="item in sliderData" :key="item.id">
+          <a href="item.linkUrl">
+            <img :src="item.picUrl" class="needsclick">
+          </a>
+        </div>
+      </slider>
+    </div>
   </div>
 </template>
 
 <script>
 import { getTopBanner } from 'api/recommend'
 import { ERR_OK } from 'api/config'
+import Slider from 'base/slider/Slider'
 export default {
   name: 'Recommend',
   data() {
     return {
-      slider: []
+      sliderData: []
     }
   },
   created() {
     this._getTopBanner()
   },
+  components: { Slider },
   methods: {
     _getTopBanner() {
       getTopBanner().then((res) => {
         if (res.code === ERR_OK) {
-          this.slider = res.data.slider
+          this.sliderData = res.data.slider
         }
       })
     }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  @import '~common/stylus/variable'
+  .recommend
+    position fixed
+    width 100%
+    top 88px
+    bottom 0
+    .slider-wrapper
+      position relative
+      width 100%
+      overflow hidden
+</style>
