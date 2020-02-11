5a8485a0cbb1c05f93b043a53039933e47f63e5f
mengxiang <1547719290@qq.com>
2020-02-11 21:59:48 +0800

    第六章完成

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -2,19 +2,35 @@
   <div class="music-list">
     <div class="back" @click="back"><i class="icon-back"></i></div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle" ref="bgImage"></div>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="filter" ref="filter"></div>
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length > 0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+    </div>
+    <div class="bg-layer" ref="layer"></div>
     <scroll class="list" ref="list" @scroll="scroll"
             :listenScroll="listenScroll" :probeType="probeType">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
     </scroll>
+    <div class="filter"></div>
   </div>
 </template>
 
 <script>
 import Scroll from 'base/scroll/Scroll'
 import SongList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
+
+const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
+
 export default {
   name: 'MusicList',
   props: {
@@ -31,6 +47,11 @@ export default {
       default: () => { return [] }
     }
   },
+  data() {
+    return {
+      scrollY: 0
+    }
+  },
   computed: {
     bgStyle() {
       return `background-image: url(${this.bgImage})`
@@ -42,15 +63,44 @@ export default {
   },
   mounted() {
     this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTransalteY = -this.imageHeight + RESERVED_HEIGHT
     this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+  },
+  watch: {
+    scrollY(newVal) {
+      let blur = 0
+      let zIndex = 0
+      let scale = 1
+      let translateY = Math.max(this.minTransalteY, newVal)
+      const percent = Math.abs(newVal / this.imageHeight)
+      if (newVal > 0) {
+        zIndex = 10
+        scale += percent
+      } else {
+        blur = Math.min(20, percent * 20)
+      }
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
+      if (newVal < this.minTransalteY) {
+        zIndex = 10
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style['transform'] = `scale(${scale})`
+    }
   },
   methods: {
     back() {
       this.$router.back()
     },
     scroll(pos) {
-      console.log(pos)
-      // const bgImage = this.$refs.bgImage
+      this.scrollY = pos.y
     }
   },
   components: {
@@ -98,6 +148,40 @@ export default {
       padding-top 70%
       background-size cover
       transform-origin top
+      .play-wrapper
+        position absolute
+        bottom 20px
+        z-index 50
+        width 100%
+        .play
+          box-sizing border-box
+          width 135px
+          padding 7px 0
+          margin 0 auto
+          text-align center
+          color: $color-theme
+          border 1px solid $color-theme
+          border-radius 100px
+          .icon-play
+            display inline-block
+            vertical-align middle
+            margin-right 6px
+            font-size: $font-size-small
+          .text
+            display inline-block
+            vertical-align middle
+            font-size $font-size-small
+      .filter
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        background: rgba(7, 17, 27, 0.4)
+    .bg-layer
+      position: relative
+      height: 100%
+      background: $color-background
     .list
       position fixed
       top 0
