29343b56f66c206941b1581dc0f695ac2c4bb60f
mengxiang <1547719290@qq.com>
2020-02-19 08:40:51 +0800

    少写了this.scroll

--- src/base/scroll/Scroll.vue
+++ src/base/scroll/Scroll.vue
@@ -89,7 +89,7 @@ export default {
       this.scroll && this.scroll.refresh()
     },
     scrollTo() {
-      this.scroll && this.scrollTo.apply(this.scroll, arguments)
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
     },
     scrollToElement() {
       this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
