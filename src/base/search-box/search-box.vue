e3ce27822727a1a5eb3c5c660b906bc697a07126
mengxiang <1547719290@qq.com>
2020-02-22 09:25:01 +0800

    search-box 完成

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box" v-model="query" :placeholder="placeholder">
+    <i @click="clear" v-show="query" class="icon-dismiss"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  data() {
+    return {
+      query: ''
+    }
+  },
+  methods: {
+    clear() {
+      this.query = ''
+    }
+  },
+  created() {
+    this.$watch('query', (newQuery) => {
+      this.$emit('query', newQuery)
+    })
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+.search-box
+  display flex
+  align-items center
+  box-sizing border-box
+  padding 0 6px
+  height 40px
+  width: 100%
+  background $color-highlight-background
+  border-radius 6px
+  .icon-search
+    font-size 24px
+    color $color-background
+  .box
+    flex 1
+    margin 0 5px
+    line-height 18px
+    background $color-highlight-background
+    color $color-text
+    font-size $font-size-medium
+    &::placeholder
+      color $color-text-d
+  .icon-dismiss
+    font-size 16px
+    color $color-background
+</style>
--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>search</div>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchBox from 'base/search-box/search-box'
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  components: {
+    SearchBox
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.search
+  .search-box-wrapper
+    margin 20px
+</style>
