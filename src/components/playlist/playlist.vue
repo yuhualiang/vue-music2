2d22394206991a10a84622a080e9087491f732e1
mengxiang <1547719290@qq.com>
2020-03-02 20:57:17 +0800

    11-10完成

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -25,7 +25,7 @@
           </transition-group>
         </scroll>
         <div class="list-operate">
-          <div class="add">
+          <div @click.stop="addSong" class="add">
             <i class="icon-add"></i>
             <span class="text">添加歌曲到队列</span>
           </div>
@@ -35,15 +35,17 @@
         </div>
       </div>
       <confirm ref="confirm" @confirm="confirmClear" text="是否清空播放列表" confirmBtnText="清空"></confirm>
+      <add-song ref="addSong"></add-song>
     </div>
   </transition>
 </template>
 <script type="text/ecmascript-6">
-import {mapGetters, mapMutations, mapActions} from 'vuex'
+import {mapMutations, mapActions} from 'vuex'
 import {playMode} from 'common/js/config'
 import Scroll from 'base/scroll/Scroll'
 import Confirm from 'base/confirm/confirm'
 import {playerMixin} from 'common/js/mixin'
+import AddSong from 'components/add-song/add-song'
 
 export default {
   mixins: [playerMixin],
@@ -102,6 +104,9 @@ export default {
       this.setCurrentIndex(index)
       this.setPlayingState(true)
     },
+    addSong() {
+      this.$refs.addSong.show()
+    },
     ...mapMutations({
       setCurrentIndex: 'SET_CURRENT_INDEX',
       setPlayingState: 'SET_PLAYING_STATE'
@@ -122,7 +127,7 @@ export default {
     }
   },
   components: {
-    Scroll, Confirm
+    Scroll, Confirm, AddSong
   }
 }
 </script>
