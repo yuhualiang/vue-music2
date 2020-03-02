4f94a20e2614ba8312d265c5d6363b3bd37a994f
mengxiang <1547719290@qq.com>
2020-03-02 20:37:30 +0800

    11-9

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,4 +1,7 @@
-import {mapGetters} from 'vuex'
+import {mapGetters, mapMutations} from 'vuex'
+import {playMode} from 'common/js/config'
+import {shuffle} from 'common/js/util'
+
 // import {createSong} from 'common/js/song'
 // import {getPurlUrl} from 'api/song' /* 获取歌曲的播放地址 */
 // import { ERR_OK } from 'api/config'
@@ -24,6 +27,46 @@ export const playlistMixin = {
     handlePlaylist() {
       throw new Error('component must implement handlePlaylist method!')
     }
+  }
+}
+
+export const playerMixin = {
+  computed: {
+    iconMode() {
+      return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' : 'icon-random'
+    },
+    ...mapGetters([
+      'sequenceList',
+      'playlist',
+      'currentSong',
+      'mode'
+    ])
+  },
+  methods: {
+    changeMode() {
+      const mode = (this.mode + 1) % 3
+      this.setPlayMode(mode)
+      let list = null
+      if (mode === playMode.random) {
+        list = shuffle(this.sequenceList)
+      } else {
+        list = this.sequenceList
+      }
+      this._resetCurrentIndex(list)
+      this.setPlayList(list)
+    },
+    _resetCurrentIndex(list) {
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id
+      })
+      this.setCurrentIndex(index)
+    },
+    ...mapMutations({
+      setPlayMode: 'SET_PLAY_MODE',
+      setPlayList: 'SET_PLAYLIST',
+      setCurrentIndex: 'SET_CURRENT_INDEX',
+      setPlayingStage: 'SET_PLAYING_STATE'
+    })
   }
 }
 
